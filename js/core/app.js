6a3d421246da01f5c81db9cc9c0319543bf7fc55
pengvench <pingvinchik211@gmail.com>
2026-05-30 17:16:07 +0700

    минорные фиксики

--- js/core/app.js
+++ js/core/app.js
@@ -10,7 +10,7 @@ let initStudioIntro = () => {};
 let initSnakePopup = () => {};
 let initShowcaseStack = () => {};
 let VideoEngine = class { async load() { return false; } start() {} };
-const ASSET_VERSION = '20260530-3';
+const ASSET_VERSION = '20260530-12';
 
 async function loadModules() {
     await Promise.allSettled([
