da5945fa1b292f34f60a00eb4ebffba550872796
pengvench <pingvinchik211@gmail.com>
2026-06-01 20:29:50 +0700

    уточнены тарифы калькулятора

--- js/core/app.js
+++ js/core/app.js
@@ -11,7 +11,7 @@ let initSnakePopup = () => {};
 let initShowcaseStack = () => {};
 let initPriceCalculator = () => {};
 let VideoEngine = class { async load() { return false; } start() {} };
-const ASSET_VERSION = '20260601-1';
+const ASSET_VERSION = '20260601-2';
 
 async function loadModules() {
     await Promise.allSettled([
